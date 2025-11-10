cddf039a5b0f53f6194326b6534155a0853c98af
Timur Ibragimov <ibragimov.tim02@gmail.com>
2025-11-10 21:30:28 +0300

    feat(ui): add index page with API levels overview (A–E)

--- polygon_token/urls.py
+++ polygon_token/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index_view
 
 urlpatterns = [
+    path("", index_view, name="index"),
     path('admin/', admin.site.urls),
     path("api/", include("token_api.urls")),
 ]
