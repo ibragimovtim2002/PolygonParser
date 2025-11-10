b084af47bdebacbbe03f712b01ad5fd6c89f8b0e
Timur Ibragimov <ibragimov.tim02@gmail.com>
2025-11-10 18:52:30 +0300

    chore(config): add POLYGONSCAN_API_KEY for API access

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ from web3 import Web3
 
 POLYGON_RPC = "https://polygon-rpc.com"
 TOKEN_ADDRESS = Web3.to_checksum_address("0x1a9b54a3075119f1546c52ca0940551a6ce5d2d0")
+POLYGONSCAN_API_KEY = "GAY4TX7BA1TNKDQ4VNE1GC984JIBZRPIFT"
